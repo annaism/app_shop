be52329aa2261fa449788895508bee56d70a4bc3
Joker <ronghao5201314@foxmail.com>
2022-11-21 19:55:50 +0800

    完成了登录和支付功能的开发

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -20,7 +20,7 @@
 
     <!-- 结算按钮 -->
     <!-- 结算按钮 -->
-    <view class="btn-settle">结算({{checkedCount}})</view>
+    <view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
   </view>
 
 </template>
@@ -28,29 +28,169 @@
 <script>
   import {
     mapGetters,
-    mapMutations
+    mapMutations,
+    mapState
   } from 'vuex'
   export default {
     computed: {
       // 1. 将 total 映射到当前组件中
       ...mapGetters('m_cart', ['total', 'checkedCount', 'checkedGoodsAmount']),
+      ...mapGetters('m_user', ['addstr']),
+      // token 是用户登录成功之后的 token 字符串
+      ...mapState('m_user', ['token']),
+      ...mapState('m_cart', ['cart']),
       // 2. 是否全选
       isFullCheck() {
         return this.total === this.checkedCount
       },
     },
     data() {
-      return {}
+      return {
+        // 倒计时的秒数
+        seconds: 3,
+        // 定时器的 Id
+        timer: null,
+
+      }
     },
     methods: {
+      // 把 m_user 模块中的 updateRedirectInfo 方法映射到当前页面中使用
+      ...mapMutations('m_user', ['updateRedirectInfo']),
       // 2. 使用 mapMutations 辅助函数，把 m_cart 模块提供的 updateAllGoodsState 方法映射到当前组件中使用
       ...mapMutations('m_cart', ['updateAllGoodsState']),
+      // 把 m_user 模块中的 updateRedirectInfo 方法映射到当前页面中使用
+        ...mapMutations('m_user', ['updateRedirectInfo']),
+      // addstr 是详细的收货地址
       // label 的点击事件处理函数
       changeAllState() {
         // 修改购物车中所有商品的选中状态
         // !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
         this.updateAllGoodsState(!this.isFullCheck)
+      },
+      // 点击了结算按钮
+      settlement() {
+        // 1. 先判断是否勾选了要结算的商品
+        if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+
+        // 2. 再判断用户是否选择了收货地址
+        if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+
+        // 3. 最后判断用户是否登录了
+        // if (!this.token) return uni.$showMsg('请先登录！')
+        if (!this.token) return this.delayNavigate()
+
+        // 4. 实现微信支付功能
+        this.payOrder()
+      },
+
+      // 展示倒计时的提示消息
+      showTips(n) {
+        // 调用 uni.showToast() 方法，展示提示消息
+        uni.showToast({
+          // 不展示任何图标
+          icon: 'none',
+          // 提示的消息
+          title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+          // 为页面添加透明遮罩，防止点击穿透
+          mask: true,
+          // 1.5 秒后自动消失
+          duration: 1500
+        })
+      },
+      // 延迟导航到 my 页面
+      delayNavigate() {
+        // 把 data 中的秒数重置成 3 秒
+        this.seconds = 3
+        this.showTips(this.seconds)
+
+        this.timer = setInterval(() => {
+          this.seconds--
+
+          if (this.seconds <= 0) {
+            // 清除定时器
+            clearInterval(this.timer)
+            // 跳转到 my 页面
+            uni.switchTab({
+              url: '/pages/my/my',
+              // 页面跳转成功之后的回调函数
+              success: () => {
+                // 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+                this.updateRedirectInfo({
+                  // 跳转的方式
+                  openType: 'switchTab',
+                  // 从哪个页面跳转过去的
+                  from: '/pages/cart/cart'
+                })
+              }
+            })
+
+            return
+          }
+
+          this.showTips(this.seconds)
+        }, 1000)
+      },
+      // 微信支付
+      async payOrder() {
+        // 1. 创建订单
+        // 1.1 组织订单的信息对象
+        const orderInfo = {
+          // 开发期间，注释掉真实的订单价格，
+          // order_price: this.checkedGoodsAmount,
+          // 写死订单总价为 1 分钱
+          order_price: 0.01,
+          consignee_addr: this.addstr,
+          goods: this.cart.filter(x => x.goods_state).map(x => ({
+            goods_id: x.goods_id,
+            goods_number: x.goods_count,
+            goods_price: x.goods_price
+          }))
+        }
+        // 1.2 发起请求创建订单
+        const {
+          data: res
+        } = await uni.$http.post('/api/public/v1/my/orders/create', orderInfo)
+        if (res.meta.status !== 200) return uni.$showMsg('创建订单失败！')
+        // 1.3 得到服务器响应的“订单编号”
+        const orderNumber = res.message.order_number
+
+        // 2. 订单预支付
+        // 2.1 发起请求获取订单的支付信息
+        const {data :res2}= await uni.$http.post('/api/public/v1/my/orders/req_unifiedorder', {
+          order_number: orderNumber
+        })
+        
+        console.log(res2)
+        // 2.2 预付订单生成失败
+        if (res2.meta.status !== 200) return uni.$showMsg('预付订单生成失败！')
+        // 2.3 得到订单支付相关的必要参数
+        const payInfo = res2.message.pay
+
+        // 3. 发起微信支付
+        // 3.1 调用 uni.requestPayment() 发起微信支付
+        const [err, succ] = await uni.requestPayment(payInfo)
+        // 3.2 未完成支付
+        if (err) return uni.$showMsg('订单未支付！')
+        // 3.3 完成了支付，进一步查询支付的结果
+        const {
+          data: res3
+        } = await uni.$http.post('/api/public/v1/my/orders/chkOrder', {
+          order_number: orderNumber
+        })
+        // 3.4 检测到订单未支付
+        if (res3.meta.status !== 200) return uni.$showMsg('订单未支付！')
+        // 3.5 检测到订单支付完成
+        uni.showToast({
+          title: '支付完成！',
+          icon: 'success'
+        })
       }
+
+
+
+
+
+
     },
   }
 </script>
@@ -89,7 +229,4 @@
       padding: 0 10px;
     }
   }
-
-
-
 </style>
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
@@ -7,7 +6,9 @@ import App from './App'
 import store from './store/store.js'
 
 //导入请求拦截器
-import { $http } from '@escook/request-miniprogram'
+import {
+  $http
+} from '@escook/request-miniprogram'
 
 Vue.config.productionTip = false
 
@@ -16,28 +17,40 @@ uni.$http = $http
 $http.baseUrl = 'https://www.uinav.com'
 
 // 请求开始之前做一些事情
-$http.beforeRequest = function (options) {
+$http.beforeRequest = function(options) {
   uni.showLoading({
     title: '数据加载中...',
   })
+
+  // 判断请求的是否为有权限的 API 接口
+  if (options.url.indexOf('/my/') !== -1) {
+    // 为请求头添加身份认证字段
+    options.header = {
+      // 字段的值可以直接从 vuex 中进行获取
+      Authorization: store.state.m_user.token,
+    }
+  }
 }
+
 // 请求完成之后做一些事情
-$http.afterRequest = function () {
+$http.afterRequest = function() {
   uni.hideLoading()
 }
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App,
-    // 2. 将 store 挂载到 Vue 实例上
-    store,
+  ...App,
+  // 2. 将 store 挂载到 Vue 实例上
+  store,
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
@@ -48,7 +61,7 @@ export function createApp() {
 // #endif
 
 // 封装的展示消息提示的方法
-uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
   uni.showToast({
     title,
     duration,
